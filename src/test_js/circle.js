ebd589d238313e4508940579df3ecae1a9e8ad10
cOsMiCTr <onurbaki@me.com>
2022-01-16 23:38:23 +0100

    Task 1.2

---
+++ src/test_js/circle.js
@@ -0,0 +1,11 @@
+
+const PI = 3.14159265359;
+export function area(radius) {
+  return radius ** 2 * PI;
+}
+export function circumference(radius) {
+  return 2 * radius * PI;
+}
+export function onurTest(radius, amount) {
+  return radius * amount;
+}
---
+++ src/test_js/main.js
@@ -0,0 +1,9 @@
+
+
+import { area, circumference, onurTest } from "./circle.js";
+let r = 3;
+let x = 10;
+console.log(
+  `Circle with radius ${r} has area: ${area(r)};
+  circumference: ${circumference(r)}
+  and this is Onurs Test result: ${onurTest(r,x)}`);
